425a173966c01de61faa583c121ab09fed65efa9
Hector GODZIEN <hgodzien@gmail.com>
2020-05-28 19:45:05 +0200

    Clean code v0

--- src/LaunchClientGUI.java
+++ src/LaunchClientGUI.java
@@ -1,8 +1,6 @@
 public class LaunchClientGUI {
-
 	public static void main(String[] args) {
 		ViewConnection view = new ViewConnection();
 		view.setPresenter(new PresenterConnection(view, new ModelConnection()));
 	}
-
 }
