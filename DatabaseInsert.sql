ee4a4f550a43dbd53dbe0baecf2e4f6a16e162e5
Rokare <maxime.abensur@gmail.com>
2020-06-01 06:00:02 +0200

    sql file for database insert

---
+++ DatabaseInsert.sql
@@ -0,0 +1,52 @@
+/*
+
+Insert for fast test
+
+*/
+
+
+INSERT INTO user
+VALUES
+	(4, "jacques", "123456", null),
+	(5, "avril", "123456", null),
+	(6, "marie", "123456", null),
+	(7, "pierre", "123456", null),
+	(8, "pegasse", "123456", null),
+	(9, "jean", "123456", null);
+
+
+INSERT INTO conversation
+VALUES
+	(1),
+	(2),
+	(3),
+	(4);
+
+INSERT INTO message
+VALUES
+	(1, "feshugjsdfhgjsdfhf", 4, 1,"2016-01-03 01:50:18"),
+	(2, "dddddddddddd", 4, 2,"2016-01-03 01:50:18"),
+	(3, "sdfszefzef", 7, 2,"2016-01-03 01:50:18"),
+	(4, "fezffsfsd", 8, 3,"2016-01-03 01:50:18"),
+	(5, "fsdf", 8, 4,"2016-01-03 01:50:18"),
+	(6, "sfdsfsfds", 8, 4,"2016-01-03 01:50:18"),
+	(7, "bbbbbbb", 4, 1,"2016-01-03 01:50:18"),
+	(8, "fffffffff", 5, 2,"2016-01-03 01:50:18"),
+	(9, "ezrezrdfff", 9, 2,"2016-01-03 01:50:18"),
+	(10, "dsfdsfsf", 1, 3,"2016-01-03 01:50:18"),
+	(11, "sdffsdfsf", 2, 4,"2016-01-03 01:50:18"),
+	(12, "eererz", 9, 4,"2016-01-03 01:50:18");
+	
+	
+commande:
+
+/*sqlite3 filename
+#.header on
+.mode  column
+for decribe table properly
+pragma table table_info('tableName');
+*/
+
+
+
+
